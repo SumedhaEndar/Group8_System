b47ffb954df257baf4594a094e63e900e4d9b2c1
SumedhaEndar <1211200546@student.mmu.edu.my>
2024-02-05 22:35:09 +0800

    Done the ProgramEnroll Done the Program Options Done the Program Details Done the Payment

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ProgramEnroll
+
+class ProgramEnrollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'member_id', 'program_id')
+    ordering = ('id',)
 
 # Register your models here.
+admin.site.register(ProgramEnroll)
--- webpages/urls.py
+++ webpages/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("plans/", views.plans, name="plans"),
     path("programs/", views.programs, name="programs"),
+    path("program-detail/<int:program_id>", views.program_detail, name="program-detail"),
+    path("plans/", views.plans, name="plans"),
     path("fitness-locator/", views.fitnessLocator, name="fitness-locator"),
     path("contact-us/", views.contactUs, name="contact-us"),
 ]
