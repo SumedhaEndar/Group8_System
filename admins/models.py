45cba614319f21fe60ca15051edcd08ea5a82edb
1211201350-YAPCHUNWEI <1211201350@student.mmu.edu.my>
2024-02-05 15:54:54 +0800

    Update: Admin Parts

--- admins/models.py
+++ admins/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import Member
 
 # Create your models here.
+class PaymentNotification(models.Model):
+    user = models.OneToOneField(Member, on_delete=models.CASCADE, primary_key=True, related_name='member')
+    message = models.CharField(default=None, max_length=255)
+    outstanding_payment = models.DecimalField(default=0, max_digits=10, decimal_places=2)
