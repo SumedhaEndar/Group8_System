8280ec34254faac0e3e6c79540885dae496626e9
Nico-Lee-Guang-Yang <1211201869@student.mmu.edu.my>
2024-02-05 10:21:00 +0800

    Update view.py and url.py about program detail and delete program Update HTML new program-detail.html and trainer-home.html change css Update model.py Fitness Program, program_time change to selections

--- trainers/models.py
+++ trainers/models.py
@@ -2,9 +2,18 @@ from django.db import models
 from users.models import Trainer
 
 class FitnessProgram(models.Model):
+    DAYS_OF_WEEK = (
+        ('MONDAY', 'Monday'),
+        ('TUESDAY', 'Tuesday'),
+        ('WEDNESDAY', 'Wednesday'),
+        ('THURSDAY', 'Thursday'),
+        ('FRIDAY', 'Friday'),
+        ('SATURDAY', 'Saturday'),
+        ('SUNDAY', 'Sunday'),
+    )
     program_id = models.AutoField(primary_key=True)
     program_name = models.CharField(max_length=100)
-    program_day = models.CharField(max_length=50)
+    program_day = models.CharField(max_length=20, choices=DAYS_OF_WEEK, default='Mon')
     program_time = models.CharField(max_length=50)
     program_pax = models.IntegerField()
     program_price = models.DecimalField(max_digits=10, decimal_places=2)
